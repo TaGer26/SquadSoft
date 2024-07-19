b89c4f12c16e97702ca977a5f8975952b0818307
sgtlomzik <75279463+sgtlomzik@users.noreply.github.com>
2024-07-19 19:57:17 +0300

    Update SquadSoft.py

--- SquadSoft.py
+++ SquadSoft.py
@@ -6,6 +6,8 @@ from keyboard import add_hotkey
 from pynput import mouse
 from pyautogui import screenshot
 import os
+from calculate_azimuth import calculate_azimuth
+from calculate_elevation import calculate_elevation
 
 main = CTk()
 main.attributes('-fullscreen', 1)
@@ -13,23 +15,27 @@ main.attributes('-topmost', 1)
 main.wm_attributes('-transparentcolor', '#242424')
 main.overrideredirect(True)
 
+
 class SquadSoft():
     def __init__(self, window):
         self.window = window
-        self.main_canvas = CTkCanvas(self.window, width=main.winfo_screenwidth(), height=main.winfo_screenheight(), bg='#242424')
+        self.main_canvas = CTkCanvas(self.window, width=main.winfo_screenwidth(), height=main.winfo_screenheight(),
+                                     bg='#242424')
         self.main_canvas.place(x=-2, y=-2)
-        self.version =0.2
+        self.version = 0.2
 
         self.load_settings()
         self.gp_table()
         self.calculate_distance()
         self.menu()
         self.zoom()
+
     def window_leave(self):
         main.quit()
         exit()
+
     def load_settings(self):
-        #load
+        # load
         self.settings = {}
         settings_path = 'settings.txt'
         if os.path.exists(settings_path):
@@ -40,24 +46,30 @@ class SquadSoft():
                         self.settings[key] = value
                     except:
                         pass
-        #keybind
+        # keybind
         add_hotkey(self.settings['window_leave'], lambda: self.window_leave())
+
     def menu(self):
         self.menu_switch = False
         self.menu_zoom_mul = 8
+        self.menu_zoom_frequency = 1
         menuF = CTkFrame(self.window, width=750, height=500)
         self.menuF_start_x, self.menuF_start_y = 300, 100
         current_x, current_y = 300, 100
+
         def move_menu(e):
             nonlocal current_x, current_y
             current_x += e.x_root - self.menuF_start_x
             current_y += e.y_root - self.menuF_start_y
             menuF.place(x=current_x, y=current_y)
             self.menuF_start_x, self.menuF_start_y = e.x_root, e.y_root
+
         def mouse_press(event):
             self.menuF_start_x, self.menuF_start_y = event.x_root, event.y_root
+
         menuF.bind("<B1-Motion>", move_menu)
         menuF.bind("<ButtonPress-1>", mouse_press)
+
         def switchF():
             if self.gp_table_switch:
                 self.gp_table_switch = False
@@ -65,22 +77,40 @@ class SquadSoft():
             else:
                 self.gp_table_switch = True
                 menuF.place(x=current_x, y=current_y)
+
         add_hotkey(self.settings['menu_switch'], switchF)
-        #menu_content
-        soft_nameL = CTkLabel(menuF, text=f'SquadSoft v{self.version}', font=('Arial Black', 12)).place(x=5, y=0)
+        # menu_content
+        soft_nameL = CTkLabel(menuF, text=f'SquadSoft v{self.version} MODDED', font=('Arial Black', 12)).place(x=5, y=0)
         gp_tableC = CTkCheckBox(menuF, text='Таблица ГП', font=('Arial Black', 12))
         gp_tableC.place(x=5, y=50)
         calculatorC = CTkCheckBox(menuF, text='Калькулятор расстояния', font=('Arial Black', 12))
         calculatorC.place(x=5, y=90)
         zoomC = CTkCheckBox(menuF, text='Приближение прицела', font=('Arial Black', 12))
         zoomC.place(x=5, y=130)
+
         def change_multiplier(value):
             zoom_multipliereL.configure(text=f'x{int(value)}')
             self.menu_zoom_mul = int(value)
+
         zoom_multiplier = CTkSlider(menuF, from_=1, to=16, number_of_steps=16, command=change_multiplier)
         zoom_multiplier.place(x=215, y=135)
         zoom_multipliereL = CTkLabel(menuF, text=f'x8', font=('Arial Black', 10))
         zoom_multipliereL.place(x=430, y=128)
+
+        def change_frequency(value):
+            zoom_frequency_label.configure(text=f'{int(value)} ms')
+            self.menu_zoom_frequency = int(value)
+
+        zoom_frequency = CTkSlider(menuF, from_=1, to=16, number_of_steps=8, command=change_frequency)
+        zoom_frequency.place(x=215, y=175)
+        zoom_frequency.set(1)
+
+        zoom_frequency_label_t = CTkLabel(menuF, text=f'Время обновления', font=('Arial Black', 12))
+        zoom_frequency_label_t.place(x=35, y=170)
+
+        zoom_frequency_label = CTkLabel(menuF, text=f'1 ms', font=('Arial Black', 10))
+        zoom_frequency_label.place(x=430, y=168)
+
         def apply_settings():
             if gp_tableC.get() == 1:
                 self.gp_state = True
@@ -95,6 +125,8 @@ class SquadSoft():
             else:
                 self.zoom_state = False
             self.zoom_m = self.menu_zoom_mul
+            self.zoom_f = self.menu_zoom_frequency
+
         applyB = CTkButton(menuF, text='Применить', font=('Arial Black', 12), command=apply_settings).place(x=5, y=460)
 
     def gp_table(self):
@@ -109,27 +141,36 @@ class SquadSoft():
                            7: {'name': 'Китай QLG10', 'path': r'GP\qlg10.png', 'size': (625, 260)},
                            8: {'name': 'США M203', 'path': r'GP\m203_usa.png', 'size': (625, 190)},
                            9: {'name': 'МПСША M203', 'path': r'GP\m203_usa+.png', 'size': (625, 220)},
-                           10: {'name': 'Турция AK40GL (ВСЁ летит левее на 2-3 градуса)', 'path': r'GP\ak40gl.png', 'size': (625, 250)},
+                           10: {'name': 'Турция AK40GL (ВСЁ летит левее на 2-3 градуса)', 'path': r'GP\ak40gl.png',
+                                'size': (625, 250)},
                            11: {'name': 'Турция MGL Барабан', 'path': r'GP\mgl.png', 'size': (625, 330)},
                            12: {'name': 'Милита FN FAL Rifle Frag', 'path': r'GP\rifle_frag.png', 'size': (625, 150)}}
         self.gp_weapon = 5
-        #gp_table_widget
-        gp_tableL = CTkLabel(self.window, image=CTkImage(dark_image=Image.open(self.gp_weapons[self.gp_weapon]['path']), size=(625*float(self.settings['gp_scale']), 330*float(self.settings['gp_scale']))), text='')
-        gp_nameL = CTkLabel(self.window, text=self.gp_weapons[self.gp_weapon]['name'], font=('Arial Black', 15), text_color=self.settings['text_color'],fg_color='#242424')
-        #move_table
+        # gp_table_widget
+        gp_tableL = CTkLabel(self.window, image=CTkImage(dark_image=Image.open(self.gp_weapons[self.gp_weapon]['path']),
+                                                         size=(625 * float(self.settings['gp_scale']),
+                                                               330 * float(self.settings['gp_scale']))), text='')
+        gp_nameL = CTkLabel(self.window, text=self.gp_weapons[self.gp_weapon]['name'], font=('Arial Black', 15),
+                            text_color=self.settings['text_color'], fg_color='#242424')
+        # move_table
         self.gp_start_x, self.gp_start_y = 25, 25
         current_x, current_y = 25, 25
+
         def move_table(e):
             nonlocal current_x, current_y
             current_x += e.x_root - self.gp_start_x
             current_y += e.y_root - self.gp_start_y
             gp_tableL.place(x=current_x, y=current_y)
-            gp_nameL.place(x=current_x, y=current_y+(self.gp_weapons[self.gp_weapon]['size'][1]*float(self.settings['gp_scale']))+10)
+            gp_nameL.place(x=current_x, y=current_y + (
+                        self.gp_weapons[self.gp_weapon]['size'][1] * float(self.settings['gp_scale'])) + 10)
             self.gp_start_x, self.gp_start_y = e.x_root, e.y_root
+
         def mouse_press(event):
-            self.gp_start_x, self.gp_start_y  = event.x_root, event.y_root
+            self.gp_start_x, self.gp_start_y = event.x_root, event.y_root
+
         gp_tableL.bind("<B1-Motion>", move_table)
         gp_tableL.bind("<ButtonPress-1>", mouse_press)
+
         # keybind
         def gp_table_switchF():
             if self.gp_state:
@@ -140,18 +181,28 @@ class SquadSoft():
                 else:
                     self.gp_table_switch = True
                     gp_tableL.place(x=current_x, y=current_y)
-                    gp_nameL.place(x=current_x, y=current_y + (self.gp_weapons[self.gp_weapon]['size'][1]*float(self.settings['gp_scale'])) + 10)
+                    gp_nameL.place(x=current_x, y=current_y + (
+                                self.gp_weapons[self.gp_weapon]['size'][1] * float(self.settings['gp_scale'])) + 10)
+
         add_hotkey(self.settings['gp_switch'], gp_table_switchF)
+
         def gp_weapon_switchF():
             if self.gp_state:
                 if self.gp_weapon < 12:
                     self.gp_weapon += 1
                 else:
                     self.gp_weapon = 1
-                gp_tableL.configure(image=CTkImage(dark_image=Image.open(self.gp_weapons[self.gp_weapon]['path']), size=((self.gp_weapons[self.gp_weapon]['size'][0]*float(self.settings['gp_scale'])), (self.gp_weapons[self.gp_weapon]['size'][1]*float(self.settings['gp_scale'])))))
+                gp_tableL.configure(image=CTkImage(dark_image=Image.open(self.gp_weapons[self.gp_weapon]['path']),
+                                                   size=((self.gp_weapons[self.gp_weapon]['size'][0] * float(
+                                                       self.settings['gp_scale'])), (
+                                                                     self.gp_weapons[self.gp_weapon]['size'][1] * float(
+                                                                 self.settings['gp_scale'])))))
                 gp_nameL.configure(text=self.gp_weapons[self.gp_weapon]['name'])
-                gp_nameL.place(x=current_x, y=current_y + (self.gp_weapons[self.gp_weapon]['size'][1]*float(self.settings['gp_scale'])) + 10)
+                gp_nameL.place(x=current_x, y=current_y + (
+                            self.gp_weapons[self.gp_weapon]['size'][1] * float(self.settings['gp_scale'])) + 10)
+
         add_hotkey(self.settings['gp_weapon_switch'], gp_weapon_switchF)
+
     def calculate_distance(self):
         self.calculate_state = True
         self.pix_distance = 0
@@ -162,6 +213,7 @@ class SquadSoft():
                 self.click_count = 0
                 x_coord = []
                 xy_coords = []
+
                 def on_click(x, y, button, pressed):
                     if button == mouse.Button.left and pressed:
                         self.click_count += 1
@@ -179,17 +231,31 @@ class SquadSoft():
                                 x2, y2 = xy_coords[1]
                                 pix_dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
                                 real_dist = round(pix_dist / (self.pix_distance / self.pnr_distance))
+                                elevation = calculate_elevation(real_dist)
+                                azimuth = calculate_azimuth(x1,y1,x2,y2)
                                 def show_distance():
-                                    line = self.main_canvas.create_line(x1, y1, x2, y2, width=4, fill=self.settings['text_color'])
-                                    distanceL = CTkLabel(self.window, text=f'{real_dist}м', font=('Arial Black', 15),text_color=self.settings['text_color'], fg_color='#242424')
-                                    distanceL.place(x=x2, y=y2+5)
+                                    line = self.main_canvas.create_line(x1, y1, x2, y2, width=4,
+                                                                        fill=self.settings['text_color'])
+                                    distanceL = CTkLabel(self.window, text=f'{real_dist} м\n{elevation} m i l\n{azimuth} °', font=('Arial Black', 15),
+                                                         text_color=self.settings['text_color'], fg_color='#242424')
+                                    distanceL.place(x=x2, y=y2 + 5)
+
+                                    distanceL2 = CTkLabel(self.window,
+                                                         text=f'{real_dist} м {elevation} mil {azimuth} °',
+                                                         font=('Arial Black', 13),
+                                                         text_color='#EEEEEE', fg_color='#242424')
+                                    distanceL2.place(x=890, y=1060)
+
                                     sleep(1.25)
                                     distanceL.destroy()
                                     self.main_canvas.delete(line)
+
                                 Thread(target=show_distance).start()
+
                 listener = mouse.Listener(on_click=on_click)
                 listener.start()
                 listener.join()
+
         def switch_distance():
             if self.calculate_state:
                 if self.pnr_distance == 100:
@@ -198,38 +264,45 @@ class SquadSoft():
                     self.pnr_distance = 900
                 else:
                     self.pnr_distance = 100
+
                 def show_distance():
-                    distanceL = CTkLabel(self.window, text=f'{self.pnr_distance}м', font=('Arial Black', 25), text_color=self.settings['text_color'], fg_color='#242424')
+                    distanceL = CTkLabel(self.window, text=f'{self.pnr_distance}м', font=('Arial Black', 25),
+                                         text_color=self.settings['text_color'], fg_color='#242424')
                     distanceL.place(relx=0.5, rely=0.5)
                     sleep(1)
                     distanceL.destroy()
+
                 Thread(target=show_distance).start()
+
         add_hotkey(self.settings['distance_start'], lambda: calculator('start'))
         add_hotkey(self.settings['distance_calculate'], lambda: calculator('real'))
         add_hotkey(self.settings['distance_start_switch'], switch_distance)
-        
+
     def zoom(self):
         self.zoom_flag = False
         self.zoom_state = True
         self.zoom_m = 2
-        def img_zoom(img, zoom):
-            w, h = img.size
-            zoom2 = zoom * 2
-            img = img.crop((960 - w / zoom2, 540 - h / zoom2,
-                            960 + w / zoom2, 540 + h / zoom2))
-            return img
+        self.zoom_f = 1
         def zoomF():
             self.zoom_flag = True
-            def update_image():
-                im = screenshot()
-                im = img_zoom(im, 12)
-                self.zoomL.configure(image=CTkImage(dark_image=im, size=(450, 300)))
-                self.window.after(16, update_image)
-
-            im = img_zoom(screenshot(), self.zoom_m)
-            imT = CTkImage(im, size=(600, 600))
+            def img_zoom(img, zoom):
+                w, h = img.size
+                zoom2 = zoom * 2
+                img = img.crop((960 - w / zoom2, 540 - h / zoom2,
+                                960 + w / zoom2, 540 + h / zoom2))
+                return img
+            im = screenshot()
+            im = img_zoom(im, self.zoom_m)
+            imT = CTkImage(im, size=(450,300))
             self.zoomL = CTkLabel(self.window, image=imT, text='')
             self.zoomL.place(x=int(self.settings['zoom_position_x']), y=int(self.settings['zoom_position_y']))
+            def update_image():
+                if self.zoom_flag:
+                    im = screenshot()
+                    im = img_zoom(im, self.zoom_m)
+                    imT = CTkImage(im, size=(450, 300))
+                    self.zoomL.configure(image=imT)
+                    self.window.after(self.zoom_f, update_image)
             update_image()
         def zoom_switch():
             if self.zoom_state:
@@ -244,6 +317,5 @@ class SquadSoft():
 
 if __name__ == '__main__':
     app = SquadSoft(main)
-
 
 main.mainloop()
